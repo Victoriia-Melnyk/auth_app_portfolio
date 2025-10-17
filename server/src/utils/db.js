c786c42cc2d5dc8e5d90932f48b79f4864aa6a11
Viktoriia Melnyk <melnychka1993@gmail.com>
2025-10-17 11:49:40 +0100

    database file changes

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,9 +1,18 @@
 import { Sequelize } from 'sequelize';
 
-export const client = new Sequelize({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  dialect: 'postgres',
+// export const client = new Sequelize({
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   dialect: 'postgres',
+// });
+
+export const client = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres',
+	protocol: 'postgres',
+	dialectOptions:
+		process.env.NODE_ENV === 'production'
+			? { ssl: { require: true, rejectUnauthorized: false } }
+			: {},
 });
